5811f87c01f88fcd806d2754f5164f00f4280913
Jose @ GCP <jose.for.dev@gmail.com>
2022-04-19 15:14:40 +0000

    Task - Add server implementation using NodeJS.

---
+++ server/src/utils/util.js
@@ -0,0 +1,88 @@
+
+const fs = require('fs');
+const { generatePiDigits } = require('./spigot'); // main algo to calculate pi, replaceable with other algo as and when needed
+const { parse, stringify } = require('envfile');
+const pathToenvFile = "./pi.config";
+
+async function addPiPrecision() {
+    let latestPiValue = "";
+    let it = generatePiDigits();
+
+    var decimals = await getDecimalValue();
+
+    // append digit to pi
+    for (let i = 0; i <= decimals; i++){
+        latestPiValue += it.next().value;
+    }
+
+    // write new config variable values
+    await setConfigValue("PI", latestPiValue);
+    await setConfigValue("DECIMAL", decimals + 1);
+
+    console.table("DEBUG: Latest PI value set to " + format(latestPiValue));
+    return latestPiValue;
+}
+
+async function getPiValue() {
+    var PI = await getConfigValue("PI");
+    return (PI); 
+}
+
+async function getDecimalValue() {
+    var DECIMAL = await getConfigValue("DECIMAL");
+    return (Number(DECIMAL)); 
+}
+
+function getConfigValue(configKey = "PI") {
+    return new Promise((resolve, reject) => {
+        fs.readFile(pathToenvFile, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+
+            var result = parse(data);
+            var configValue = result[configKey].toString();
+            //console.log(configKey + ":" + configValue);
+
+            if (configKey === "PI") {
+                resolve(configValue.length === 1 ? configValue : format(configValue)); 
+            }
+            else
+            {
+                resolve(Number(configValue));
+            }
+        });
+    });
+}
+
+function setConfigValue(key, value) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(pathToenvFile, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            var result = parse(data);
+            result[key] = value;
+            //console.table("DEBUG:" + JSON.stringify(result));
+            fs.writeFile(pathToenvFile, stringify(result), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                //console.log("File Saved");
+                resolve();
+            })
+
+        });
+    });
+}
+
+// format pi with decimal value
+function format(str) {
+    return str.substring(0, 1) + "." + str.substring(1, str.length);
+}
+
+module.exports = {
+    addPiPrecision,
+    getPiValue,
+    format
+}
